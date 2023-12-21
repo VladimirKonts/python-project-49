e87ab947bc8f4beab0586d12ba2ad84a03954781
vladimir.konts@yandex.ru <root@User-PC.localdomain>
2023-12-21 20:20:39 +0300

    шаг 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from ast import main
+from cli import welcome_user
 
 
 def show_greeting():
     text = 'Welcome to the Brain Games!'
     print(text)
 
-show_greeting()    
+show_greeting()
+
+welcome_user()
 
 if __name__ == '__main__':
     main()
---
+++ brain_games/scripts/cli.py
@@ -0,0 +1,3 @@
+from prompt_toolkit import prompt
+
+name = prompt('May I have your name? ')
